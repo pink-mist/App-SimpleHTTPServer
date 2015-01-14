1131839018643f3b20dd94b010128fc5a80e5086
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2015-01-14 13:42:59 +0100

    Initial commit

---
+++ bin/serve_dir.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+
+use App::SimpleHTTPServer @ARGV;
+
+# Really, there's not much more to this...
---
+++ lib/App/SimpleHTTPServer.pm
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+package App::SimpleHTTPServer;
+
+# ABSTRACT: Serve up a directory via http simply and easily
+use Mojolicious::Lite;
+use Scalar::Util qw/ looks_like_number /;
+
+sub import {
+    my $package = shift;
+    my $port    = shift;
+    if (not looks_like_number $port) {
+        unshift @ARGV, $port if defined $port;
+        $port   = 8000;
+    }
+    my $path    = shift;
+       $path    = '.' unless defined $path;
+
+    plugin Directory => root => $path;
+
+    app->start( qw/ daemon -m production -l /, "http://*:$port/" );
+}
+
+1;
